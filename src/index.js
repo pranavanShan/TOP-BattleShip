b8bf9627dd0eed4d384ab004bb31e879183f580a
miamam1 <theshpeecrab@gmail.com>
2024-08-03 17:08:06 +0100

    Added all skeleton code / configuration files required outside of jest. Using ESLint and webpack.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+import "./ships.js";
+//use ESLint! (Right click should show reformat doc, make sure to have VS code install ESLint)
+//If you get an error saying ./src/src couldn't be found, you've used npx webpack in the wrong directory, needs to be in the project folder, not src.
+//Need to implement Jest 
+// TDD boils down to:
+// Red: Make a test, make it fail
+// Yellow: Make a solution that passes the test, no matter how ugly or wrong the code is
+// Green: Refactor the code to make it functional 
+// Repeat until program is done
