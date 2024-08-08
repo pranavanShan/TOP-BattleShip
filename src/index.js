78993946fc71ae30e182ac19e899e8dc76875f48
miamam1 <theshpeecrab@gmail.com>
2024-08-08 18:31:35 +0100

    Added robot moves, whjich activate after user has inputted their hit. No game finish logic implemented yet.

--- src/index.js
+++ src/index.js
@@ -24,8 +24,6 @@ player2.computerPlace();
 //jest doesnt work cus of style.css
 //dosnt rly matter cus told not to test css anyway so
 // need to find out how to make robot to moves and have it rejister
-//cant rly do in create player grid cus its being made in the function n shit
-//which was probabl rly dumb, might need to sepreate create grid and attackplayer logic even if it is DOM related.
 function createPlayerGrid(player) {
   const playerGrid = document.createElement("div");
   playerGrid.classList.add("gameboard");
@@ -34,24 +32,6 @@ function createPlayerGrid(player) {
     for (let j = 0; j < player.board.board[i].length; j++) {
       const part = document.createElement("div");
       part.classList.add("part");
-      part.addEventListener("click", () => {
-        player.board.receiveAttack([j, i]);
-        if (player.board.board[j][i] === -2) {
-          part.style.backgroundColor = "red";
-          part.classList.add("inactivePart");
-        }
-        if (player.board.board[j][i] === -1) {
-          part.style.backgroundColor = "grey";
-          part.classList.add("inactivePart");
-        }
-        document
-          .querySelectorAll(".inactiveBoard")[0]
-          .classList.remove("inactiveBoard");
-        playerGrid.classList.add("inactiveBoard");
-        if (player.board.lost) {
-          //lose logic here
-        }
-      });
       row.append(part);
       playerGrid.append(row);
     }
@@ -59,12 +39,80 @@ function createPlayerGrid(player) {
   return playerGrid;
 }
 
+function receieveAttackDOM(player, playerGrid) {
+  for (let i = 0; i < player.board.board.length; i++) {
+    for (let j = 0; j < player.board.board[i].length; j++) {
+      let currentNode = playerGrid.childNodes[i].childNodes[j];
+      currentNode.addEventListener("click", () => {
+        player.board.receiveAttack([j, i]);
+        currentNode.classList.add("inactivePart");
+        if (player.board.board[j][i] == -1)
+          currentNode.style.backgroundColor = "grey";
+        if (player.board.board[j][i] == -2)
+          currentNode.style.backgroundColor = "red";
+        if (player.board.lost) console.log("Lost!"); //Lose logic
+      });
+    }
+  }
+}
+
+
+function robotAttackDOM(player, playerGrid) {
+  let coords = [player.getRandomInteger(0,10), player.getRandomInteger(0,10)]
+  let result = player.board.receiveAttack(coords)
+  while(result === false) {
+    coords = [player.getRandomInteger(0,10), player.getRandomInteger(0,10)]
+    result = player.board.receiveAttack(coords)
+  }
+  let [y, x] = [...coords]
+  if(player.board.board[y][x] == -1) {
+    playerGrid.childNodes[x].childNodes[y].style.backgroundColor = "grey"
+  }
+  if(player.board.board[y][x] == -2) {
+    playerGrid.childNodes[x].childNodes[y].style.backgroundColor = "red"
+  }
+
+  if(player.board.lost) console.log("lost!")
+
+  return true
+}
+
+
+
+function turnController(player1Grid, player2Grid) {
+  for (let i = 0; i < player1Grid.childNodes.length; i++) {
+    for (let j = 0; j < player1Grid.childNodes[i].childNodes.length; j++) {
+      player1Grid.childNodes[i].childNodes[j].addEventListener("click", () => {
+        changeTurns(player1Grid, player2Grid);
+        
+      });
+      player2Grid.childNodes[i].childNodes[j].addEventListener("click", () => {
+        robotAttackDOM(player1, player1Grid)
+        changeTurns(player1Grid, player2Grid)
+        changeTurns(player1Grid, player2Grid);
+      });
+    }
+  }
+}
+
+function changeTurns(playerGrid, player2Grid) {
+  if (playerGrid.classList.contains("inactiveBoard")) {
+    playerGrid.classList.remove("inactiveBoard");
+    player2Grid.classList.add("inactiveBoard");
+  } else if (player2Grid.classList.contains("inactiveBoard")) {
+    player2Grid.classList.remove("inactiveBoard");
+    playerGrid.classList.add("inactiveBoard");
+  }
+}
+
 const player1Grid = createPlayerGrid(player1);
 const player2Grid = createPlayerGrid(player2);
+receieveAttackDOM(player1, player1Grid, player2, player2Grid);
+receieveAttackDOM(player2, player2Grid, player1, player1Grid);
 player1Grid.classList.add("inactiveBoard");
+turnController(player1Grid, player2Grid);
 document.body.append(player1Grid);
 document.body.append(player2Grid);
-
 export { Ship, gameBoard, player };
 
 /* 
@@ -82,3 +130,26 @@ export { Ship, gameBoard, player };
  Y
 board [y][x]
 */
+
+/*
+part.addEventListener("click", () => {
+        player.board.receiveAttack([j, i]);
+        if (player.board.board[j][i] === -2) {
+          part.style.backgroundColor = "red";
+          part.classList.add("inactivePart");
+        }
+        if (player.board.board[j][i] === -1) {
+          part.style.backgroundColor = "grey";
+          part.classList.add("inactivePart");
+        }
+        document
+          .querySelectorAll(".inactiveBoard")[0]
+          .classList.remove("inactiveBoard");
+        playerGrid.classList.add("inactiveBoard");
+        if (player.board.lost) {
+          //lose logic here
+        }
+      });
+
+
+      */
